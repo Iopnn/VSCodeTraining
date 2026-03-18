604b74491c91d10067056e05d8d8765bfb958251
Nicolas Chartier <leptitchartier@gmail.com>
2026-03-18 00:44:46 -0400

    1.0.6

--- Blabla.py
+++ Blabla.py
@@ -5,27 +5,31 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
 @bot.command()
-async def help(ctx):
+async def commands_list(ctx):
     await ctx.send("1- brainrot, 2- alpasin, 3- fuckjews, 4- meray, fuck niggers")
 
+@bot.command()
 async def brainrot(ctx):
     brainrotList = ["Meray", "Popoy", "Alpasin", "Alpablanc", "Berere"]
     brainrotFinal = random.choice(brainrotList)
     await ctx.send(f"Your random number is {brainrotFinal}")
 
+@bot.command()
 async def alpasin(ctx):
     await ctx.send("Fuck les alpasin et les alpablanc")
 
+@bot.command()
 async def fuckjews(ctx):
     await ctx.send("Fuck the fucking jews")
 
+@bot.command()
 async def meray(ctx):
     await ctx.send("Popoy ay ay magazine")
 
